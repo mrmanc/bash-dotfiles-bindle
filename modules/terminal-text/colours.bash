08168c5a79145143396c5cc075e992eafccadc4e
Mark Crossfield <mark@markcrossfield.co.uk>
2014-01-20 18:45:06 +0000

    Funky colourful text.

---
+++ modules/terminal-text/colours.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+# See http://bit.ly/terminal-colours
+export BLACK='\033[0;30m'
+export BLUE='\033[0;34m'
+export GREEN='\033[0;32m'
+export CYAN='\033[0;36m'
+export RED='\033[0;31m'
+export PURPLE='\033[0;35m'
+export YELLOW='\033[0;33m'
+export WHITE='\033[1;37m'
+export RESET_TEXT='\033[0m'
