df5f932c1914f76a0ea1bf248b29cadf7f588d09
Mark Crossfield <github@nanunanu.co.uk>
2014-01-20 23:41:59 +0000

    More precise detection of previous install.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 BINDLE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if ! [ -f ~/.bash_profile ]; then touch ~/.bash_profile; fi
 bindle_hook=". $BINDLE_PATH/bindle_profile"
-if ! grep -q "$bindle_hook" ~/.bash_profile
+if ! grep -q "^$bindle_hook$" ~/.bash_profile
 then
 	echo $bindle_hook >> ~/.bash_profile
         echo -e "\033[0;32mHooked dotfiles in by adding a line to ~/.bash_profile\033[0m"
