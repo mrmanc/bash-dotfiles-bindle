468e7ac3bea99df66d1248ed9d61c1795d6cdefe
Mark Crossfield <mark@markcrossfield.co.uk>
2014-01-21 09:56:26 +0000

    Keeping track of typos for future analysis.

---
+++ modules/history/misspelled-commands.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Keep a note of commands you can't type for use in future aliases
+
+function err_handle {
+  status=$?
+  if [[ $status -eq 127 ]]; then
+    read date time cmd args <<< $(history | tail -1 | sed 's/^ *[0-9]* *//')
+    echo $cmd >> .bash_misspelled_command_history
+  fi
+}
+trap 'err_handle' ERR
