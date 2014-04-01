b797a5ca0083b579f80363fc28def10652d9c0c3
Mark Crossfield <mark@markcrossfield.co.uk>
2014-04-01 17:31:38 +0100

    Improving formatting of prompt.

--- modules/terminal-text/prompt.bash
+++ modules/terminal-text/prompt.bash
@@ -2,10 +2,10 @@
 systemSymbol=""
 case "$(uname -s)" in
 	Linux)
-		systemSymbol=" 🐧 "
+		systemSymbol=" 🐧"
   		;;
 	Darwin)
-		systemSymbol="$GREEN  $RESET_TEXT"
+		systemSymbol="$GREEN $RESET_TEXT"
 		;;
 esac
 export PS1="$WHITE\u@\h${PROMPT_HOME_IDENTIFIER}${systemSymbol} $BLUE\w$RESET_TEXT \[$YELLOW\]$\[$RESET_TEXT\] "
