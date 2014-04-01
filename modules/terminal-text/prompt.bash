1e70afa52a7b68664e673264bb9698eecac30f5f
Mark Crossfield <mark@markcrossfield.co.uk>
2014-04-01 17:19:50 +0100

    Adding OS symbol to prompt

--- modules/terminal-text/prompt.bash
+++ modules/terminal-text/prompt.bash
@@ -1,2 +1,11 @@
 #!/bin/bash
-export PS1="$WHITE[\u@\h$PROMPT_HOST_SUFFIX]$RESET_TEXT $BLUE\w$RESET_TEXT \[$YELLOW\]$\[$RESET_TEXT\] "
+systemSymbol=""
+case "$(uname -s)" in
+	Linux)
+		systemSymbol=" 🐧 "
+  		;;
+	Darwin)
+		systemSymbol="$GREEN  $RESET_TEXT"
+		;;
+esac
+export PS1="$WHITE[\u@\h${PROMPT_HOST_SUFFIX}${systemSymbol}]$RESET_TEXT $BLUE\w$RESET_TEXT \[$YELLOW\]$\[$RESET_TEXT\] "
