ee884fc73f03137835c026e71b17c10677c707f7
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2014-08-22 16:26:13 +0100

    Added a newline to the prompt to give more space.

--- modules/terminal-text/prompt.bash
+++ modules/terminal-text/prompt.bash
@@ -8,4 +8,4 @@ case "$(uname -s)" in
 		systemSymbol="$GREEN $RESET_TEXT"
 		;;
 esac
-export PS1="$WHITE\u@\h${PROMPT_HOME_IDENTIFIER}${systemSymbol} $BLUE\w$RESET_TEXT \[$YELLOW\]$\[$RESET_TEXT\] "
+export PS1="$WHITE\u@\h${PROMPT_HOME_IDENTIFIER}${systemSymbol} $BLUE\w$RESET_TEXT \[$YELLOW\]\n$\[$RESET_TEXT\] "
