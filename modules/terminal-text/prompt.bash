363ea118b0344b84afccce9b259ceb27ca881912
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2015-09-18 00:06:52 +0100

    Adds a 💥  to the prompt when the status of the last command is non zero

--- modules/terminal-text/prompt.bash
+++ modules/terminal-text/prompt.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+status=$?
 systemSymbol=""
 case "$(uname -s)" in
 	Linux)
@@ -9,4 +10,4 @@ case "$(uname -s)" in
 		;;
 esac
 # Handy information: http://www.thegeekstuff.com/2008/09/bash-shell-ps1-10-examples-to-make-your-linux-prompt-like-angelina-jolie/
-export PS1="${WHITE}\u@\h${PROMPT_HOME_IDENTIFIER}${systemSymbol} ${WHITE}\e[44m\A${RESET_TEXT} ${BLUE}\w${RESET_TEXT}\[${YELLOW}\]\n$\[${RESET_TEXT}\] "
+export PS1="${WHITE}\u@\h${PROMPT_HOME_IDENTIFIER}${systemSymbol} ${WHITE}\e[44m\A${RESET_TEXT} ${BLUE}\w${RESET_TEXT}\[${YELLOW}\]\n\$(if [ \$? -ne 0 ]; then echo 💥\  ; else echo \$; fi;)\[${RESET_TEXT}\] "
