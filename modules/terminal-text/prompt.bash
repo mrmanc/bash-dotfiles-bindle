1ecd8fef378d46a1295d47f61bf818f645aeab5c
Mark Crossfield <github@nanunanu.co.uk>
2015-09-22 21:24:33 +0100

    Further emojification of status indicator in prompt.

--- modules/terminal-text/prompt.bash
+++ modules/terminal-text/prompt.bash
@@ -9,5 +9,7 @@ case "$(uname -s)" in
 		systemSymbol="$GREEN $RESET_TEXT"
 		;;
 esac
+okayStatusIndicator=😍
+errorStatusIndicator=💩
 # Handy information: http://www.thegeekstuff.com/2008/09/bash-shell-ps1-10-examples-to-make-your-linux-prompt-like-angelina-jolie/
-export PS1="${WHITE}\u@\h${PROMPT_HOME_IDENTIFIER}${systemSymbol} ${WHITE}\e[44m\A${RESET_TEXT} ${BLUE}\w${RESET_TEXT}\[${YELLOW}\]\n\$(if [ \$? -ne 0 ]; then echo 💥\  ; else echo \$; fi;)\[${RESET_TEXT}\] "
+export PS1="${WHITE}\u@\h${PROMPT_HOME_IDENTIFIER}${systemSymbol} ${WHITE}\e[44m\A${RESET_TEXT} ${BLUE}\w${RESET_TEXT}\[${YELLOW}\]\n\$(if [ \$? -ne 0 ]; then echo ${errorStatusIndicator}; else echo ${okayStatusIndicator}; fi;) \[${RESET_TEXT}\] "
