4830bf0130c1a5acf8ec6f9de2075ca9d3bb973d
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2016-04-14 11:49:51 +0100

    Makes prompt command easier to understand.

--- modules/terminal-text/prompt.bash
+++ modules/terminal-text/prompt.bash
@@ -5,13 +5,13 @@ case "$(uname -s)" in
 		systemSymbol=" 🐧 "
   		;;
 	Darwin)
-		systemSymbol="$GREEN $RESET_TEXT"
+		systemSymbol="${GREEN} ${RESET_TEXT}"
 		;;
 esac
-okayStatusIndicator=😍
-errorStatusIndicator=💩
 # Handy information: http://www.thegeekstuff.com/2008/09/bash-shell-ps1-10-examples-to-make-your-linux-prompt-like-angelina-jolie/
-CURRENT_TIME="$WHITE\e[44m\A$RESET_TEXT"
-CURRENT_PATH="$BLUE\w$RESET_TEXT"
-USER_AT_HOST="$WHITE\u@\h"
-export PS1="$USER_AT_HOST${PROMPT_HOME_IDENTIFIER}${systemSymbol} $CURRENT_TIME $CURRENT_PATH\n$(if [ $? -ne 0 ]; then echo $errorStatusIndicator; else echo $okayStatusIndicator; fi;) \[$RESET_TEXT\]"
+okayStatusIndicator="😍"
+errorStatusIndicator="💩"
+currentTime="${WHITE}\e[44m\A${RESET_TEXT}"
+currentPath="${BLUE}\w${RESET_TEXT}"
+userAtHost="${WHITE}\u@\h"
+export PS1="${userAtHost}${PROMPT_HOME_IDENTIFIER}${systemSymbol} ${currentTime} ${currentPath}\n\$(if [ \$? -ne 0 ]; then echo ${errorStatusIndicator}; else echo ${okayStatusIndicator}; fi;) \[${RESET_TEXT}\] "
